721bbfd26a9aad81da7bd76aff600100bfa0ec28
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-06-27 16:13:23 -0400

    banana skyscraper gorilla

---
+++ Matching Game EchoAR/Assets/Clicked.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class Clicked : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 1000000000f;
+
+    public string lastName = "banana";
+
+    public Camera fpsCam;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Debug.Log("apple");
+                Debug.Log(lastName);
+                if (hit.collider.gameObject.name.Equals(lastName))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+                lastName = hit.collider.gameObject.name;
+            }
+        }
+    }
+}
---
+++ Matching Game EchoAR/Assets/ClickedObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickedObject : MonoBehaviour
+{
+    public GameObject lastObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                
+                
+                if (lastObject.tag == hit.collider.gameObject.tag && lastObject != hit.collider.gameObject)
+                {
+                    //Debug.Log("Destroy");
+                    Destroy(lastObject);
+                    Destroy(hit.collider.gameObject);
+                    lastObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                }
+                else
+                {
+                    lastObject = hit.collider.gameObject;
+
+                }
+            }
+        }
+    }
+}
