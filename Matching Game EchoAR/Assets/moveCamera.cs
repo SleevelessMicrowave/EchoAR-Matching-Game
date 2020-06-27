a4438f0964791825be088c777bb2e66b601b858e
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-06-27 19:58:55 -0400

    rotate camera

---
+++ Matching Game EchoAR/Assets/moveCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+
+    public Camera mainCam;
+    
+    public KeyCode right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(right))
+        {
+            transform.RotateAround(new Vector3(0.59f, 27.7f, 6.9f), Vector3.up, 1000 * Time.deltaTime);
+        }
+    }
+}
