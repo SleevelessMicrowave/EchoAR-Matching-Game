498943574086772cb45c9ee4b57092378c76eb13
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-06-27 20:39:43 -0400

    done

--- Matching Game EchoAR/Assets/ClickedObject.cs
+++ Matching Game EchoAR/Assets/ClickedObject.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ClickedObject : MonoBehaviour
 {
     public GameObject lastObject;
+    //private int counter = 0;
+    //private int anotherCounter = 0;
 
 
     // Start is called before the first frame update
@@ -24,7 +26,8 @@ public class ClickedObject : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.collider.gameObject.name);
-                
+                GameObject cover = hit.collider.gameObject;
+                cover.GetComponent<Renderer>().enabled = false;
                 
                 if (lastObject.tag == hit.collider.gameObject.tag && lastObject != hit.collider.gameObject)
                 {
@@ -32,11 +35,23 @@ public class ClickedObject : MonoBehaviour
                     Destroy(lastObject);
                     Destroy(hit.collider.gameObject);
                     lastObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    
                 }
+                /*else if (counter % 2 == 1)
+                {
+                    lastObject.GetComponent<Renderer>().enabled = true;
+                    counter++;
+                    anotherCounter++;
+                }*/
                 else
                 {
+                    /*if (anotherCounter % 2 == 1)
+                    {
+                        cover.GetComponent<Renderer>().enabled = true;
+                    }*/
+                    lastObject.GetComponent<Renderer>().enabled = true;
                     lastObject = hit.collider.gameObject;
-
+                    
                 }
             }
         }
